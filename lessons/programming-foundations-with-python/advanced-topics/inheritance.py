33e2b7c64dfe4aedd83983eb7062b303b6e26829
elam03 <elam03@gmail.com>
2016-09-03 18:53:36 -0700

    Python inheritance lesson

--- lessons/programming-foundations-with-python/advanced-topics/inheritance.py
+++ lessons/programming-foundations-with-python/advanced-topics/inheritance.py
@@ -4,15 +4,25 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+    def show_info(self):
+        print('Last Name - ' + self.last_name)
+        print('Eye Color - ' + self.eye_color)
+
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
         print('Child Constructor Called')
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
 
-billy_cyrus = Parent('Cyrus', 'blue')
-print(billy_cyrus.last_name)
+    def show_info(self):
+        # super.show_info(self)
+        Parent.show_info(self)
+        print('Number of Toys - ' + str(self.number_of_toys))
 
+billy_cyrus = Parent('Cyrus', 'blue')
 miley_cyrus = Child('Cyrus', 'Blue', 5)
-print(miley_cyrus.last_name)
-print(miley_cyrus.number_of_toys)
+
+print('')
+billy_cyrus.show_info()
+print('')
+miley_cyrus.show_info()
